ae834ada24924911f96c86eb4b53b2a235115dca
Rahul K <rahul.kaliavaradharajulu@standardbank.co.za>
2018-12-22 16:09:28 +0200

    Removing Badge repository

--- src/main/java/com/doppler/services/UserService.java
+++ src/main/java/com/doppler/services/UserService.java
@@ -1,31 +1,19 @@
 package com.doppler.services;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 import com.doppler.entities.Badge;
-import com.doppler.entities.Reward;
-import com.doppler.entities.Topic;
 import com.doppler.entities.User;
-import com.doppler.entities.UserBadge;
 import com.doppler.entities.UserRewardPoint;
 import com.doppler.entities.UserSetting;
 import com.doppler.entities.requests.UserSearchRequest;
 import com.doppler.entities.responses.SearchResponse;
-import com.doppler.repositories.BadgeRepository;
-import com.doppler.repositories.UserBadgeRepository;
 import com.doppler.repositories.UserRepository;
 import com.doppler.repositories.UserRewardPointRepository;
 import com.doppler.security.SecurityUtils;
@@ -45,25 +33,11 @@ public class UserService extends BaseService {
 	private UserRepository userRepository;
 
 	/**
-	 * The badge repository.
-	 */
-	@Autowired
-	private BadgeRepository badgeRepository;
-
-	/**
-	 * The user badge repository.
-	 */
-	@Autowired
-	private UserBadgeRepository userBadgeRepository;
-
-	/**
 	 * The user reward point repository.
 	 */
 	@Autowired
 	private UserRewardPointRepository userRewardPointRepository;
 
-	private RestTemplate restTemplate = new RestTemplate();
-	
 	@Autowired
 	private BackendAPIService backendAPIService;
 
@@ -89,8 +63,7 @@ public class UserService extends BaseService {
 	/**
 	 * Update user setting.
 	 * 
-	 * @param userSetting
-	 *            the user setting
+	 * @param userSetting the user setting
 	 * @return the updated user setting
 	 */
 	public UserSetting updateSetting(UserSetting userSetting) {
@@ -104,8 +77,7 @@ public class UserService extends BaseService {
 	/**
 	 * Search users.
 	 *
-	 * @param criteria
-	 *            the search criteria
+	 * @param criteria the search criteria
 	 * @return the search result
 	 */
 	@Transactional(readOnly = true)
@@ -125,27 +97,6 @@ public class UserService extends BaseService {
 	}
 
 	/**
-	 * Get the badges of current logged-in user.
-	 * 
-	 * @return the badges
-	 */
-	@Transactional(readOnly = true)
-	public List<Badge> getAllCurrentUserBadges() {
-		List<Badge> badges = badgeRepository.findAll();
-		List<UserBadge> userBadges = userBadgeRepository.findByUserId(SecurityUtils.getCurrentUser().getId());
-
-		badges.forEach(badge -> {
-			userBadges.forEach(userBadge -> {
-				if (userBadge.getBadgeId().equals(badge.getId())) {
-					badge.setGainedAt(userBadge.getGainedAt());
-				}
-			});
-		});
-
-		return badges;
-	}
-
-	/**
 	 * Get the reward points of current logged-in user.
 	 * 
 	 * @return the reward points
@@ -154,18 +105,24 @@ public class UserService extends BaseService {
 	public List<UserRewardPoint> getAllCurrentUserRewardPoints() {
 		return userRewardPointRepository.findByUserId(SecurityUtils.getCurrentUser().getId());
 	}
-	
-	
 
-	
+	/**
+	 * Get the badges of current logged-in user.
+	 * 
+	 * @return the badges
+	 */
+	@Transactional(readOnly = true)
 	public List<Badge> getBadgesForCurrentUser() {
 		return backendAPIService.getForList(Badge.class, ApiConstants.BASE_URI + BADGE_URL);
 	}
-	
+
+	/**
+	 * Get the reward points of current logged-in user.
+	 * 
+	 * @return the reward points
+	 */
+	@Transactional(readOnly = true)
 	public List<UserRewardPoint> getRewardsForCurrentUser() {
-		String token = loginService.userLogin();
-		ResponseEntity<List> response = restTemplate.exchange(ApiConstants.BASE_URI + REWARD_URL, HttpMethod.GET,
-				constructHttpEntityWithRequestHeaders(token), List.class);
-		return (List<UserRewardPoint>) response.getBody();
+		return backendAPIService.getForList(UserRewardPoint.class, ApiConstants.BASE_URI + REWARD_URL);
 	}
 }
